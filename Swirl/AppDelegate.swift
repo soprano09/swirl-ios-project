c87bb3931c1a20e806c8cba259be0ce5ce9bc35a
soprano09 <topdev0804@gmail.com>
2017-09-08 16:19:03 -0700

    Added swiftline run phase and fixed lint warnings

--- Swirl/AppDelegate.swift
+++ Swirl/AppDelegate.swift
@@ -12,7 +12,8 @@ import UIKit
 final class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions
+        launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 
         return true
     }
