a792205c6980030bc14864ed668728ce62395184
soprano09 <topdev0804@gmail.com>
2017-09-11 18:39:34 -0700

    Added Facebook blue and used in for the button

--- Swirl/Extensions/UIColor+Extensions.swift
+++ Swirl/Extensions/UIColor+Extensions.swift
@@ -11,6 +11,7 @@ import UIKit
 extension UIColor {
     static var veryLightGray: UIColor { return UIColor(r: 234, g: 234, b: 234, a: 1) }
     static var lightBlue: UIColor { return UIColor(r: 168, g: 198, b: 255, a: 1) }
+    static var facebookBlue: UIColor { return UIColor(r: 66, g: 103, b: 178, a: 1) }
 
     private convenience init(r: CGFloat, g: CGFloat, b: CGFloat, a: CGFloat) { // swiftlint:disable:this identifier_name
         self.init(red: r/255, green: g/255, blue: b/255, alpha: a)
