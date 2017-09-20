f7d9206e7ac103e21df4ed448696f4dfa8cb28ca
soprano09 <topdev0804@gmail.com>
2017-09-20 14:39:07 -0700

    Added downArrow and flipCamera imageSets and added them in UIImage extensions

--- Swirl/Extensions/UIImage+Extensions.swift
+++ Swirl/Extensions/UIImage+Extensions.swift
@@ -10,13 +10,16 @@ import UIKit
 
 extension UIImage {
     enum AssetIdentifier: String {
-        /** These cases are grouped as a reminder:
+        /** The top cases are grouped as a reminder:
          *  that they are used as the tab bar items **/
         case createPost, curate, discover, following, profile
+
         case swirlEmoji
         case swirlButton
         case icon
         case settings
+        case downArrow
+        case flipCamera
     }
 
     convenience init?(asset: AssetIdentifier) {
